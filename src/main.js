f0597a54999dc33c658cd1a00786ea101759e8bd
weilaim <879181054@qq.com>
2022-07-13 12:15:55 +0800

    集成了pinia

--- src/main.js
+++ src/main.js
@@ -3,10 +3,11 @@ import 'uno.css'
 
 import { createApp } from 'vue'
 import { setupRouter } from '@/router'
+import { setupStore } from '@/store'
 import App from './App.vue'
 
 const app = createApp(App)
-
+setupStore(app)
 setupRouter(app)
 
 app.mount('#app')
