c84e56ea80912cc468aefa313b965474e793a58f
weilaim <879181054@qq.com>
2022-07-13 11:24:24 +0800

    集成插件配置和unocss

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-
+import '@/styles/index.scss'
+import 'uno.css'
 createApp(App).mount('#app')
--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,16 @@
 import { defineConfig, loadEnv } from 'vite'
-import vue from '@vitejs/plugin-vue'
 import path from 'path'
 
 import { wrapperEnv, createProxy } from './build/utils'
-
+import { createVitePlugins } from './build/plugin'
 export default defineConfig(({ command, mode }) => {
+  const isBuild = command === 'build'
   const env = loadEnv(mode, process.cwd())
   const viteEnv = wrapperEnv(env)
   const { VITE_PORT, VITE_PUBLIC_PATH, VITE_PROXY } = viteEnv
 
   return {
-    plugins: [vue()],
+    plugins: createVitePlugins(viteEnv, isBuild),
     base: VITE_PUBLIC_PATH || '/',
     resolve: {
       // 设置别名
