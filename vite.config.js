a44400d85e5bcf585a87d1a2112d11b54f902b96
weilaim <879181054@qq.com>
2022-07-13 11:57:37 +0800

    添加了axios和api的封装

--- vite.config.js
+++ vite.config.js
@@ -27,10 +27,9 @@ export default defineConfig(({ command, mode }) => {
       },
     },
     server: {
-      host: '0.0.0.0',  // 默认为'127.0.0.1'，如果将此设置为 `0.0.0.0` 或者 `true` 将监听所有地址，包括局域网和公网地址
-      port: VITE_PORT,  // 端口
+      host: '0.0.0.0', // 默认为'127.0.0.1'，如果将此设置为 `0.0.0.0` 或者 `true` 将监听所有地址，包括局域网和公网地址
+      port: VITE_PORT, // 端口
       proxy: createProxy(VITE_PROXY), // 代理
-    }
+    },
   }
 })
-
