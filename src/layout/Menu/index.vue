cca444ab2d450fb77b36103f9cb3896a9703a6cd
weilaim <879181054@qq.com>
2022-07-15 11:05:45 +0800

    菜单栏的图标和对应各个页面的路由

--- src/layout/Menu/index.vue
+++ src/layout/Menu/index.vue
@@ -1,10 +1,11 @@
 <template>
   <el-menu
-    active-text-color="#ffffff"
+    active-text-color="#ffd04b"
     background-color="#304156"
     class="el-menu-vertical-demo"
-    default-active="2"
+    :default-active="defaultActive"
     text-color="#fff"
+    unique-opened
     h-full
     router
     @open="handleOpen"
@@ -12,12 +13,24 @@
   >
     <el-sub-menu v-for="(item, index) in menusList" :key="item.id" :index="index">
       <template #title>
-        <el-icon><location /></el-icon>
+        <el-icon>
+          <component :is="iconlist[index]"></component>
+        </el-icon>
         <span>{{ item.authName }}</span>
       </template>
-      <el-menu-item v-for="(cmenu, cindex) in item.children" :key="cindex" :index="`/` + cmenu.path">{{
-        cmenu.authName
-      }}</el-menu-item>
+      <el-menu-item
+        v-for="(cmenu, cindex) in item.children"
+        :key="cindex"
+        :index="`/` + cmenu.path"
+        @click="savePath(cmenu.path)"
+      >
+        <template #title>
+          <el-icon>
+            <component :is="icon"></component>
+          </el-icon>
+          <span>{{ cmenu.authName }}</span>
+        </template>
+      </el-menu-item>
     </el-sub-menu>
   </el-menu>
 </template>
@@ -26,12 +39,18 @@
 import { ref } from 'vue'
 import { menuList } from '../../api/menu'
 
+const iconlist = ref(['user', 'setting', 'shop', 'tickets', 'pie-chart'])
+const icon = ref('menu')
+const defaultActive = ref(sessionStorage.getItem('path') || '/users')
 const menusList = ref([])
 const initMenusList = async () => {
   const res = await menuList()
   menusList.value = res.data
 }
 
+const savePath = (path) => {
+  sessionStorage.setItem('path', `/${path}`)
+}
 initMenusList()
 </script>
 
--- src/main.js
+++ src/main.js
@@ -5,8 +5,11 @@ import { createApp } from 'vue'
 import { setupRouter } from '@/router'
 import { setupStore } from '@/store'
 import App from './App.vue'
-
+import * as ELIcon from '@element-plus/icons-vue'
 const app = createApp(App)
+for (const iconname in ELIcon) {
+  app.component(iconname, ELIcon[iconname])
+}
 setupStore(app)
 setupRouter(app)
 
